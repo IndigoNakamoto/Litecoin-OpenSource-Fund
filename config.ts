3ea1bf33cb33010bcee8557e6c884d43ebc21564
indigo <indigonakamoto@gmail.com>
2023-09-20 18:37:02 -0700

    Updated min amount

--- config.ts
+++ config.ts
@@ -1,8 +1,8 @@
 //config.ts
 module.exports = {
   CURRENCY: 'LTC',
-  MIN_AMOUNT: 0.25,
-  MAX_AMOUNT: 100,
+  MIN_AMOUNT: 0.01,
+  MAX_AMOUNT: 1000,
   BTCPAY_URL: 'https://ltcpay.lite.space',
   BTCPAY_STORE_ID: '3WsWRhg4KAd9EQAHExHozUiCLuH7PPWsx1FDkXfQV5gE',
 }
