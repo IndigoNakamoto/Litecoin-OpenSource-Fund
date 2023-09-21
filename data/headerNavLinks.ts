57ac011a1f45be13a73c113cbaaf092e115e1709
indigo <indigonakamoto@gmail.com>
2023-09-21 14:17:24 -0700

    Remove Blog and Apply links from header navigation
    Comment out the 'Blog' and 'Apply' links from the headerNavLinks array to temporarily hide them from the website's navigation header.

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,9 +2,9 @@ const headerNavLinks = [
   { href: '/mission', title: 'Mission' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
-  { href: '/blog', title: 'Blog' },
+  // { href: '/blog', title: 'Blog' },
   { href: '/faq', title: 'FAQ' },
-  { href: '/apply', title: 'Apply', isButton: true },
+  // { href: '/apply', title: 'Apply', isButton: true },
 ]
 
 export default headerNavLinks
