daed9155001fd3d78504128824e424431da0f250
indigo <indigonakamoto@gmail.com>
2023-09-20 18:23:08 -0700

    Add `config.ts` for application configuration:
    - Define default currency as LTC.
    - Set minimum and maximum allowable amounts.
    - Specify BTCPay URL and store ID.

---
+++ config.ts
@@ -0,0 +1,8 @@
+//config.ts
+module.exports = {
+  CURRENCY: 'LTC',
+  MIN_AMOUNT: 0.25,
+  MAX_AMOUNT: 100,
+  BTCPAY_URL: 'https://ltcpay.lite.space',
+  BTCPAY_STORE_ID: '3WsWRhg4KAd9EQAHExHozUiCLuH7PPWsx1FDkXfQV5gE',
+}
