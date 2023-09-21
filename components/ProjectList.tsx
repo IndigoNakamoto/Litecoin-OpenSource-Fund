52e5d907e18fd69e54b3301ec7e66f66c4e2f399
indigo <indigonakamoto@gmail.com>
2023-09-21 14:17:45 -0700

    Increased the number of projects shown on index from 3 to 6

--- components/ProjectList.tsx
+++ components/ProjectList.tsx
@@ -29,7 +29,7 @@ const ProjectList: React.FC<ProjectListProps> = ({
     <section className="bg-light items-left flex flex-col">
       <ul className="grid max-w-5xl gap-4 md:grid-cols-3">
         {sortedProjects &&
-          sortedProjects.slice(0, 3).map((p, i) => (
+          sortedProjects.slice(0, 6).map((p, i) => (
             <li key={i} className="">
               <ProjectCard project={p} openPaymentModal={openPaymentModal} />
             </li>
