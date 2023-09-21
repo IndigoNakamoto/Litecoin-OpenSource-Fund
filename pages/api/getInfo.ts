39974e5d0a6647b288f0d6092803604c501e9b73
indigo <indigonakamoto@gmail.com>
2023-09-20 18:22:12 -0700

    Add new API endpoint `getInfo.ts`:
    - Endpoint retrieves and filters BTCPay invoices by order ID.
    - Computes and returns the total sum of filtered invoice amounts.
    - Uses basic authentication for BTCPay requests.
    - Handles errors gracefully and logs environment variables for debugging.

---
+++ pages/api/getInfo.ts
@@ -0,0 +1,56 @@
+import { NextApiRequest, NextApiResponse } from 'next/types'
+import { fetchGetJSONAuthed } from '../../utils/api-helpers'
+
+function filterByOrderId(items, targetOrderId) {
+  return items.filter(
+    (item) =>
+      item.metadata &&
+      item.metadata.posData &&
+      item.metadata.posData.orderId === targetOrderId
+  )
+}
+
+function sumAmounts(items) {
+  return items.reduce((acc, item) => acc + Number(item.amount), 0)
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === 'GET') {
+    const slug = req.query.slug as string
+    if (!slug) {
+      return res.status(400).json({ message: 'Slug is required' })
+    }
+
+    const username = process.env.BTCPAY_USERNAME
+    const password = process.env.BTCPAY_PASSWORD
+
+    const base64Credentials = Buffer.from(username + ':' + password).toString(
+      'base64'
+    )
+
+    const auth = `Basic ${base64Credentials}`
+
+    try {
+      const response = await fetchGetJSONAuthed(
+        `${process.env.BTCPAY_URL}/stores/${process.env.BTCPAY_STORE_ID}/invoices?status=Settled`,
+        auth
+      )
+
+      const filteredInvoices = filterByOrderId(response, slug)
+      const totalAmount = sumAmounts(filteredInvoices)
+
+      res.status(200).json({
+        funded_txo_sum: totalAmount,
+        tx_count: Object.keys(filteredInvoices).length,
+      })
+    } catch (err) {
+      res.status(500).json({ statusCode: 500, message: (err as Error).message })
+    }
+  } else {
+    res.setHeader('Allow', 'GET')
+    res.status(405).end('Method Not Allowed')
+  }
+}
