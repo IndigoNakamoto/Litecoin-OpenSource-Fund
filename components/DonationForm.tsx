dcd017d17b5cb3a24056429dddbf495be6baeaec
indigo <indigonakamoto@gmail.com>
2023-09-26 14:08:32 -0700

    Introduce Email and Twitter Username Validation
    Description:
    Added regular expressions to validate email and Twitter username input in the DonationForm component. Implemented error state and messages to provide user feedback on incorrect input. Adjusted styles and markup for better UX. Also formatted the Twitter payload to ensure a complete URL is sent to the server.
    
    Key Changes:
    
    Added EMAIL_REGEX and TWITTER_USERNAME_REGEX for input validation.
    Implemented validateEmail and validateTwitter functions to check input against regular expressions.
    Created emailError and twitterError states to handle error messages.
    Updated the UI to display error messages and highlight erroneous fields.
    Modified Twitter payload formation to ensure a complete URL is sent to the server.
    Made minor UI adjustments including updating placeholders, adjusting input padding, and text color changes for better user experience.

--- components/DonationForm.tsx
+++ components/DonationForm.tsx
@@ -9,6 +9,10 @@ import Image from 'next/legacy/image'
 
 import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false
+const EMAIL_REGEX = /^$|^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
+// const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
+const TWITTER_USERNAME_REGEX = /^(?:[a-zA-Z0-9_]+)?$/
 
 type DonationStepsProps = {
   projectNamePretty: string
@@ -31,6 +35,30 @@ const DonationSteps: React.FC<DonationStepsProps> = ({
   const [btcPayLoading, setBtcpayLoading] = useState(false)
 
   const formRef = useRef<HTMLFormElement | null>(null)
+
+  // States for input validation
+  const [emailError, setEmailError] = useState('')
+  const [twitterError, setTwitterError] = useState('')
+
+  // Email validation
+  const validateEmail = (value: string) => {
+    if (!EMAIL_REGEX.test(value)) {
+      setEmailError('Invalid email address')
+      return false
+    }
+    setEmailError('')
+    return true
+  }
+
+  // Twitter validation
+  const validateTwitter = (value: string) => {
+    if (!TWITTER_USERNAME_REGEX.test(value)) {
+      setTwitterError('Invalid username. Only a-z, 0-9 and _ are allowed.')
+      return false
+    }
+    setTwitterError('')
+    return true
+  }
 
   useEffect(() => {
     let btcValid: boolean
@@ -62,7 +90,7 @@ const DonationSteps: React.FC<DonationStepsProps> = ({
       }
 
       if (twitter) {
-        Object.assign(payload, { twitter })
+        Object.assign(payload, { twitter: `www.twitter.com/${twitter}` })
       }
 
       const data = await fetchPostJSON(`/api/btcpay`, payload)
@@ -86,36 +114,67 @@ const DonationSteps: React.FC<DonationStepsProps> = ({
       onSubmit={(e) => e.preventDefault()}
     >
       <section className="flex flex-col gap-1">
-        <h3>
-          Name <span className="text-subtle">(optional)</span>
-        </h3>
-        <input
-          type="text"
-          placeholder={'Satoshi Nakamoto'}
-          required={deductable === 'yes'}
-          onChange={(e) => setName(e.target.value)}
-          className="mb-4 mt-1 block w-full rounded-md border-gray-300 text-black shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
-        ></input>
-        <h3>
-          Twitter <span className="text-subtle">(optional)</span>
-        </h3>
-        <input
-          type="text"
-          placeholder={'twitter.com/ltcfoundation'}
-          required={deductable === 'yes'}
-          onChange={(e) => setTwitter(e.target.value)}
-          className="mb-4 mt-1 block w-full rounded-md border-gray-300 text-black shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
-        ></input>
-        <h3>
-          Email <span className="text-subtle">(optional)</span>
-        </h3>
-        <input
-          type="email"
-          placeholder={`support@ltcfoundation.com`}
-          className="mt-1 block w-full rounded-md border-gray-300 text-black shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
-          required={deductable === 'yes'}
-          onChange={(e) => setEmail(e.target.value)}
-        ></input>
+        <div className="mb-4">
+          <h3>
+            Name <span className="font-light text-gray-400">(optional)</span>
+          </h3>
+          <input
+            type="text"
+            placeholder={'Satoshi Nakamoto'}
+            required={deductable === 'yes'}
+            onChange={(e) => setName(e.target.value)}
+            className=" mt-1 block w-full rounded-md border-gray-300 text-black shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+          ></input>
+        </div>
+        <div className="mb-4">
+          <h3>
+            X <span className="font-light text-gray-400">(optional)</span>{' '}
+            {/* Changed Twitter to X */}
+          </h3>
+          <div className="relative">
+            <span className="absolute left-2 top-2 text-gray-400">@</span>
+            <input
+              type="text"
+              placeholder={'username'} // Placeholder changed
+              required={deductable === 'yes'}
+              onChange={(e) => {
+                setTwitter(e.target.value)
+                validateTwitter(e.target.value)
+              }}
+              className={` mt-1 block w-full rounded-md border-gray-300 text-black shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 ${
+                twitterError ? 'border-red-500' : ''
+              }`}
+              style={{ paddingLeft: '24px' }}
+            ></input>
+          </div>
+          {twitterError && (
+            <div className="mt-0">
+              <small className="text-red-500">{twitterError}</small>
+            </div>
+          )}
+        </div>
+        <div className="mb-4">
+          <h3>
+            Email <span className="font-light text-gray-400">(optional)</span>
+          </h3>
+          <input
+            type="email"
+            placeholder={`support@ltcfoundation.com`}
+            className={`mt-1 block w-full rounded-md border-gray-300 text-black shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 ${
+              emailError ? 'border-red-500' : ''
+            }`}
+            required={deductable === 'yes'}
+            onChange={(e) => {
+              setEmail(e.target.value.trim())
+              validateEmail(e.target.value)
+            }}
+          ></input>
+          {emailError && (
+            <div className="mt-0">
+              <small className="text-red-500">{emailError}</small>
+            </div>
+          )}
+        </div>
       </section>
 
       <div className="flex flex-wrap items-center justify-center gap-4">
