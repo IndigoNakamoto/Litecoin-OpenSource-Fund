ba8f5ec3f316255e1c1144140e650eaf82c29469
indigo <indigonakamoto@gmail.com>
2023-10-04 08:55:54 -0700

    Fix TypeScript error in TwitterComments component

--- components/TwitterComments.tsx
+++ components/TwitterComments.tsx
@@ -1,8 +1,24 @@
 // components/TwitterComments.tsx
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import TweetEmbed from 'react-tweet-embed'
 
 const TwitterComments = ({ hashtag }) => {
-  const [tweets, setTweets] = useState(null)
+  const [tweets, setTweets] = useState<string[] | null>(null)
+
+  /*
+  Example tweets:  [
+    '1709187570351943763',
+    '1709071167544332364',
+    '1708663790307319882',
+    '1708478191633846424',
+    '1708323788386172977',
+    '1708216564531536163',
+    '1708205070267879899',
+    '1708179619533254919',
+    '1708155923170984321',
+    '1707708093566423133'
+  ]
+*/
 
   useEffect(() => {
     // Fetch tweets from your backend or a service
@@ -14,10 +30,9 @@ const TwitterComments = ({ hashtag }) => {
   return (
     <div>
       {tweets ? (
-        tweets.map((tweet) => (
-          <div key={tweet.id}>
-            {/* Render your tweet data */}
-            <p>{tweet.text}</p>
+        tweets.map((tweetId) => (
+          <div key={tweetId}>
+            <TweetEmbed tweetId={tweetId} />
           </div>
         ))
       ) : (
