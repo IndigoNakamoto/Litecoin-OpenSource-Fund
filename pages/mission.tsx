acaf35b0635b3d81942972c8edead9193ab094ab
indigo <indigonakamoto@gmail.com>
2023-10-07 10:05:55 -0700

    Fix mission component

--- pages/mission.tsx
+++ pages/mission.tsx
@@ -27,12 +27,12 @@ export default function Mission({
           className="h-full w-full object-cover"
         />
       </div>
-      <div className="absolute inset-0 z-10 flex items-center justify-center">
-        <div className="container mx-auto px-6">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 xl:col-span-2">
+      <div className="absolute inset-x-0 bottom-0 top-[100px] z-10 flex items-center justify-center">
+        <div className="container px-6">
+          <h1 className="text-center text-3xl font-extrabold leading-9 tracking-tight text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 xl:col-span-2">
             Mission
           </h1>
-          <h2 className="mb-4 text-xl font-bold leading-normal md:text-5xl">
+          <h2 className="mb-4 text-center text-xl font-bold leading-normal text-gray-100 md:text-5xl">
             Promote the adoption, education & development of Litecoin (LTC)
           </h2>
         </div>
