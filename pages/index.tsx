0aac26c9848d6f6490d24e5a24ed6d8922c24a89
indigo <indigonakamoto@gmail.com>
2023-10-09 14:00:47 -0700

    Update home page style

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 
 // const DEFAULT_LAYOUT = 'ListLayout'
 const FULL_SCREEN_IMAGE = '/static/images/lite-space-bg.jpg'
+const COIN = '/litecoin-svg/L-white.png'
 
 export const getStaticProps = async () => {
   // const page = allPages.find((p) => p.slug === 'mission')
@@ -28,12 +29,20 @@ export default function Mission({
           objectFit="cover"
         />
       </div>
-      <div className="z-9 absolute inset-x-0 bottom-0 top-[600px] flex items-center justify-center">
+      <div className="relative inset-x-0 bottom-0 top-[200px] z-10 flex flex-col items-center justify-center  md:absolute md:bottom-20 lg:bottom-24 xl:bottom-32">
+        <Image
+          src={COIN}
+          alt=""
+          width={300}
+          height={300}
+          objectFit="cover"
+          className="mb-36"
+        />
         <div className="container px-6">
-          <h1 className="text-6xl font-extrabold leading-9 tracking-tight text-gray-100 sm:text-6xl sm:leading-10 md:text-6xl md:leading-14 lg:text-8xl xl:col-span-2">
+          <h1 className="text-center text-6xl font-extrabold leading-9 tracking-tight text-gray-100 sm:text-6xl sm:leading-10 md:text-6xl md:leading-14 lg:text-7xl xl:col-span-2">
             Mission
           </h1>
-          <h2 className="mb-4 text-3xl font-bold leading-normal text-gray-100 md:text-4xl lg:text-5xl">
+          <h2 className="mb-4 text-center text-3xl font-bold leading-normal text-gray-100 md:text-4xl lg:text-4xl">
             Promote the adoption, education & development of Litecoin (LTC)
           </h2>
         </div>
