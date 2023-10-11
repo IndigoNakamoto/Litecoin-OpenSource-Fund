cdfa6d097d32cf9bde7f81c71ed67d9cc86452bd
indigo <indigonakamoto@gmail.com>
2023-10-11 09:18:22 -0700

    Add Barlow Semi Condensed font imports and integrate with Next.js font system

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,6 +15,92 @@ import siteMetadata from '@/data/siteMetadata'
 import { Analytics } from '@vercel/analytics/react'
 import { SearchProvider } from 'pliny/search'
 import LayoutWrapper from '@/components/LayoutWrapper'
+import localFont from 'next/font/local'
+
+const barlowSemiCondensed = localFont({
+  src: [
+    {
+      path: '../public/fonts/Barlow_Semi_Condensed/BarlowSemiCondensed-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Barlow_Semi_Condensed/BarlowSemiCondensed-Italic.ttf',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: '../public/fonts/Barlow_Semi_Condensed/BarlowSemiCondensed-Thin.ttf',
+      weight: '100',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Barlow_Semi_Condensed/BarlowSemiCondensed-ThinItalic.ttf',
+      weight: '100',
+      style: 'italic',
+    },
+    {
+      path: '../public/fonts/Barlow_Semi_Condensed/BarlowSemiCondensed-Light.ttf',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Barlow_Semi_Condensed/BarlowSemiCondensed-LightItalic.ttf',
+      weight: '300',
+      style: 'italic',
+    },
+    {
+      path: '../public/fonts/Barlow_Semi_Condensed/BarlowSemiCondensed-Medium.ttf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Barlow_Semi_Condensed/BarlowSemiCondensed-MediumItalic.ttf',
+      weight: '500',
+      style: 'italic',
+    },
+    {
+      path: '../public/fonts/Barlow_Semi_Condensed/BarlowSemiCondensed-SemiBold.ttf',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Barlow_Semi_Condensed/BarlowSemiCondensed-SemiBoldItalic.ttf',
+      weight: '600',
+      style: 'italic',
+    },
+    {
+      path: '../public/fonts/Barlow_Semi_Condensed/BarlowSemiCondensed-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Barlow_Semi_Condensed/BarlowSemiCondensed-BoldItalic.ttf',
+      weight: '700',
+      style: 'italic',
+    },
+    {
+      path: '../public/fonts/Barlow_Semi_Condensed/BarlowSemiCondensed-ExtraBold.ttf',
+      weight: '800',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Barlow_Semi_Condensed/BarlowSemiCondensed-ExtraBoldItalic.ttf',
+      weight: '800',
+      style: 'italic',
+    },
+    {
+      path: '../public/fonts/Barlow_Semi_Condensed/BarlowSemiCondensed-Black.ttf',
+      weight: '900',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Barlow_Semi_Condensed/BarlowSemiCondensed-BlackItalic.ttf',
+      weight: '900',
+      style: 'italic',
+    },
+  ],
+})
 
 export default function App({
   Component,
@@ -31,10 +117,12 @@ export default function App({
         {/* @ts-ignore */}
         <Analytics analyticsConfig={...pageProps} />
         <LayoutWrapper>
-          {/* @ts-ignore */}
-          <SearchProvider searchConfig={siteMetadata.search}>
-            <Component {...pageProps} />
-          </SearchProvider>
+          <div className={barlowSemiCondensed.className}>
+            {/* @ts-ignore */}
+            <SearchProvider searchConfig={siteMetadata.search}>
+              <Component {...pageProps} />
+            </SearchProvider>
+          </div>
         </LayoutWrapper>
       </SessionProvider>{' '}
       {/* <-- Close the wrapper here */}
