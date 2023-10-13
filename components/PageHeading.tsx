8d00e79a4ddcd2fd7daf1da0d0414d9a313d258c
indigo <indigonakamoto@gmail.com>
2023-10-12 21:16:30 -0700

    Misc ui changes

--- components/PageHeading.tsx
+++ components/PageHeading.tsx
@@ -10,7 +10,7 @@ export default function PageHeading({ title, children }: Props) {
   return (
     <div className="divide-gray-200 dark:divide-gray-700">
       <div className="">
-        <h1 className="mt-10 font-semibold leading-9 tracking-tight text-gray-900 dark:text-gray-100 xs:text-4xl sm:leading-10 md:text-7xl md:leading-14">
+        <h1 className="mt-10 font-semibold leading-9 tracking-tight text-gray-900 dark:text-gray-100 xs:text-6xl sm:leading-10 md:text-7xl md:leading-14">
           {title}
         </h1>
       </div>
