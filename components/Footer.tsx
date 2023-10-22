d71641e489a26ab877b332b5b4271ad8197fb5cf
indigo <indigonakamoto@gmail.com>
2023-10-21 18:29:01 -0700

    Add padding top footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,8 +4,8 @@ import SocialIcon from '@/components/social-icons'
 
 export default function Footer() {
   return (
-    <footer className="font-barlow-semi-condensed">
-      <div className="z-9 flex h-32 flex-col items-center">
+    <footer className="pt-8 font-barlow-semi-condensed">
+      <div className="z-9 flex h-32 flex-col items-center pt-4">
         <div className="my-3 flex space-x-5">
           <SocialIcon
             kind="mail"
