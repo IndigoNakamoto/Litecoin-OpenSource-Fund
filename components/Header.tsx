80499d711874fef041bb887b055653f8395fd836
indigo <indigonakamoto@gmail.com>
2023-11-05 13:01:49 -0800

    Update logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,17 +14,20 @@ const Header = () => {
     <header className="z-9 flex items-center justify-between py-0 font-barlow-semi-condensed text-gray-800 dark:text-gray-200">
       <div className="pt-2">
         <Link href="/" aria-label={siteMetadata.headerTitle}>
-          <div className="flex items-center">
+          <div className="flex h-20 items-center">
             <div className="mr-3">
-              <Logo className="h-20" />
+              <Logo className="" />
             </div>
-            {typeof siteMetadata.headerTitle === 'string' ? (
-              <div className="hidden h-9 text-3xl font-semibold sm:block">
-                {siteMetadata.headerTitle}
-              </div>
-            ) : (
-              siteMetadata.headerTitle
-            )}
+
+            <div className="text-2xl font-semibold sm:block">
+              <span style={{ display: 'block' }}>Lite</span>
+              <span
+                className="text-blue-500"
+                style={{ display: 'block', marginTop: '-0.4em' }}
+              >
+                .Space
+              </span>
+            </div>
           </div>
         </Link>
       </div>
