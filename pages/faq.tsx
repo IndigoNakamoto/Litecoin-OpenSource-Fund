62635bb8c1325b3e69027776c574fae4b16bbacc
indigo <indigonakamoto@gmail.com>
2023-11-07 15:51:30 -0800

    Fix FAQ Responsiveness

--- pages/faq.tsx
+++ pages/faq.tsx
@@ -25,7 +25,7 @@ export default function FAQ({
       <h1 className="pl-4 pt-20 text-5xl font-semibold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:leading-10 md:text-7xl md:leading-14 xl:col-span-2">
         FAQ
       </h1>
-      <div className="mt-0 rounded-xl bg-gradient-to-r from-gray-100 to-gray-300 p-4 dark:bg-gradient-to-r dark:from-gray-700 dark:to-gray-800 dark:text-gray-300 md:px-4 lg:px-8 xs:px-1">
+      <div className="mt-0 rounded-xl bg-gradient-to-r from-gray-100 to-gray-300 p-4 dark:bg-gradient-to-r dark:from-gray-700 dark:to-gray-800 dark:text-gray-300 md:px-4 lg:px-8">
         <FAQSection faqCategories={faqDataModule.questionsAndAnswers} />
       </div>
     </div>
