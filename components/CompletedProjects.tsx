97521606d8188307fd40ac5949c4b841880bba1e
indigo <indigonakamoto@gmail.com>
2023-11-11 23:06:25 -0800

    Refactor CompletedProjects for Enhanced SEO and Accessibility

--- components/CompletedProjects.tsx
+++ components/CompletedProjects.tsx
@@ -1,13 +1,12 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React from 'react'
 import Image from 'next/image'
-import Typing from './Typing'
 import TypingScroll from './TypingScroll'
 import Link from './Link'
 
 // Assuming you have images in the /public/static/images/projects/completed/ directory
 const imageDirectory = '/static/images/projects/completed/'
 const images = [
-  // You should list your images here
+  // List your images with descriptive alt text for SEO
   { title: 'Litecoin Core', src: 'core.png', href: '/missions/core' },
   {
     title: 'Litecoin Space',
@@ -22,51 +21,54 @@ const images = [
   { title: 'MWEB', src: 'MWEB.webp', href: '/missions/mweb' },
   { title: 'Litecoin Dev Kit', src: 'ldk.png', href: '/missions/ldk' },
   { title: 'Omni Lite', src: 'omnilite3.png', href: '/missions/omnilite' },
-
-  // ...other images
 ]
 
 function CompletedProjects() {
   return (
-    <div className="flex flex-col  pt-40  text-gray-800">
+    <section className="flex flex-col pt-40 text-gray-800">
       <div className="mx-auto flex flex-col items-center">
-        <h1 className="text-5xl font-semibold tracking-tight text-gray-800 dark:text-white md:text-4xl lg:text-7xl">
+        <h2 className="text-5xl font-semibold tracking-tight text-gray-800 dark:text-white md:text-4xl lg:text-7xl">
           We Help Advance
-        </h1>
-        <h1 className="text-4xl font-semibold text-gray-800">
+        </h2>
+        <h3 className="text-4xl font-semibold text-gray-800">
           <TypingScroll />
-        </h1>
+        </h3>
         <p className="pt-2 text-2xl text-gray-600 dark:text-gray-400">
           Unite. Fund. Innovate. - The Litecoin Development Portal
         </p>
-        <button className="mb-10 mt-8 w-40 rounded-full bg-blue-500 px-4 py-2 font-semibold transition duration-300 hover:bg-blue-400">
-          <Link href="/missions"> Explore More</Link>
-        </button>
+        <Link
+          href="/missions"
+          className="mb-10 mt-8 w-40 rounded-full bg-blue-500 px-9 py-2 font-semibold text-white transition duration-300 hover:bg-blue-400"
+        >
+          Explore More
+        </Link>
       </div>
 
       <div className="grid grid-cols-2 gap-4 md:grid-cols-3">
         {images.map((image) => (
           <div key={image.title} className="relative h-48">
-            <Link href={image.href}>
+            <Link
+              href={image.href}
+              aria-label={`Read more about ${image.title}`}
+            >
               <div className="image-wrapper relative h-full w-full overflow-hidden">
                 <Image
-                  alt={image.title}
+                  alt={`Image of ${image.title}`}
                   src={`${imageDirectory}${image.src}`}
                   layout="fill"
                   objectFit="cover"
                   objectPosition="center"
                   className="transform rounded-xl transition duration-300 ease-in-out hover:scale-105"
                 />
-
-                <h2 className="image-title absolute inset-0 flex items-center justify-center rounded-xl bg-black bg-opacity-0 text-4xl font-semibold text-white opacity-0 transition-opacity duration-300 ease-in-out hover:bg-opacity-80 hover:opacity-100">
+                <h4 className="image-title absolute inset-0 flex items-center justify-center rounded-xl bg-black bg-opacity-0 text-4xl font-semibold text-white opacity-0 transition-opacity duration-300 ease-in-out hover:bg-opacity-80 hover:opacity-100">
                   {image.title}
-                </h2>
+                </h4>
               </div>
             </Link>
           </div>
         ))}
       </div>
-    </div>
+    </section>
   )
 }
 
