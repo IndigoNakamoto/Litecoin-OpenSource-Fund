9b292fcf473d27a2b56cc4217f991688dfa77703
indigo <indigonakamoto@gmail.com>
2024-01-25 17:55:30 -0800

    Removed omnilite mission

--- components/CompletedProjects.tsx
+++ components/CompletedProjects.tsx
@@ -24,7 +24,6 @@ const images = [
     src: 'ldk.png',
     href: '/missions/litecoin_dev_kit',
   },
-  { title: 'Omni Lite', src: 'omnilite3.png', href: '/missions/omnilite' },
   { title: 'Litescribe', src: 'litescribe3.png', href: '/missions/litescribe' },
   {
     title: 'Litewallet',
