b95684ab5c1fdb65ae1952b3d594ffb2c4f7ebcf
indigo <indigonakamoto@gmail.com>
2024-08-07 10:47:33 -0700

    Add console log debug overpaid transactions

--- pages/api/getInfo.ts
+++ pages/api/getInfo.ts
@@ -15,6 +15,9 @@ function getTwitterSupporters(items) {
 }
 
 function sumAmounts(items) {
+  // TODO: Fix amount for overpaid invoices
+  console.log('sumAmounts for these items (Todo: Fix amount for overpaid):')
+  items.forEach((item) => console.log(item))
   return items.reduce((acc, item) => acc + Number(item.amount), 0)
 }
 
