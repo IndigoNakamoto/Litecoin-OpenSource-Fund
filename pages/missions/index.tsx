1210ca6b08073df833d74b1b525fd8b2de53ce8c
indigo <indigonakamoto@gmail.com>
2024-08-09 18:32:11 -0700

    Remove header of missions page

--- pages/missions/index.tsx
+++ pages/missions/index.tsx
@@ -83,44 +83,6 @@ const AllProjects: NextPage<{ projects: ProjectItem[] }> = ({ projects }) => {
         <title>Lite.Space | Explore</title>
       </Head>
 
-      <section className="mt-8">
-        <div className="relative h-96 overflow-hidden rounded-xl border-2 dark:border-gray-700">
-          <Image
-            src="/static/images/hero.jpg"
-            layout="fill"
-            objectFit="cover"
-            alt="Lite.Space Hero Image"
-            priority={true}
-            className="absolute left-0 top-0 z-0 h-full w-full"
-          />
-          {/* Text content */}
-          <div className="z-1 absolute left-0 top-0 flex h-full w-full flex-col justify-center  bg-black bg-opacity-40 px-4">
-            <div className="flex items-center">
-              {' '}
-              {/* Add this line to create a flex container */}
-              <h1 className="flex-grow text-5xl font-semibold leading-9 tracking-tight text-gray-100 sm:leading-10 md:text-7xl md:leading-14 xl:col-span-2">
-                Explore Missions
-              </h1>
-              {/* Position LitecoinIcon next to the h1 */}
-              <div className="flex h-full md:mr-20 xs:mr-4">
-                {' '}
-                {/* Added styles to vertically center and give spacing */}
-                {/* TODO: LitecoinIcon not rendering */}
-                <LitecoinIcon kind="coinBlueL" />
-              </div>
-            </div>
-            <div className="space-y-0 pb-0 pt-4 md:space-y-0">
-              <h1 className="pb-2 text-2xl font-semibold leading-9 tracking-tight text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14 xs:text-3xl">
-                Supporting <Typing />
-              </h1>
-              <p className="mb-0 pt-32 text-2xl font-medium leading-7 text-gray-100">
-                {siteMetadata.description}
-              </p>
-            </div>
-          </div>
-        </div>
-      </section>
-
       <div className="space-y-2 pb-0 pt-10 md:space-y-5 xl:grid xl:grid-cols-3 xl:gap-x-8">
         <h2 className="pl-4 pt-10 text-5xl font-semibold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:leading-10 md:text-7xl md:leading-14 xl:col-span-2">
           Open-Source Projects
@@ -128,7 +90,7 @@ const AllProjects: NextPage<{ projects: ProjectItem[] }> = ({ projects }) => {
       </div>
       <section className="flex flex-col rounded-3xl bg-gradient-to-b from-gray-200 to-gray-300 p-4 dark:from-gray-700 dark:to-gray-600 md:p-8">
         <div className="flex w-full items-center justify-between"></div>
-        <ul className="grid max-w-5xl gap-8 lg:grid-cols-2 xl:grid-cols-3">
+        <ul className="grid max-w-5xl gap-8">
           {openSourceProjects &&
             openSourceProjects.map((p, i) => (
               <li key={i} className="">
