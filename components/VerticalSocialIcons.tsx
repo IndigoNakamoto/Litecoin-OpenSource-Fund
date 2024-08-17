842b7fe812f16826f5e1d265213f5e296eb1fb2c
indigo <indigonakamoto@gmail.com>
2024-08-17 15:49:25 -0700

    Hide vertical social icons below lg

--- components/VerticalSocialIcons.tsx
+++ components/VerticalSocialIcons.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import FacebookIcon from '@/components/social-icons-mobile/facebook.svg'
 import GithubIcon from '@/components/social-icons-mobile/github.svg'
 import TwitterIcon from '@/components/social-icons-mobile/twitter.svg'
+// import XIcon from '@/components/social-icons-mobile/x.svg'
 import RedditIcon from '@/components/social-icons-mobile/reddit.svg'
 
 interface VerticalSocialIconsProps {
@@ -13,7 +14,7 @@ const VerticalSocialIcons: React.FC<VerticalSocialIconsProps> = ({
 }) => {
   return (
     <div
-      className="fixed right-0 z-50 transform space-x-16" // Adjust the gap here
+      className="fixed right-0 z-50 hidden transform space-x-16 lg:block"
       style={{ top: topOffset, transform: 'translateY(-50%)' }}
     >
       <a
@@ -21,24 +22,24 @@ const VerticalSocialIcons: React.FC<VerticalSocialIconsProps> = ({
         target="_blank"
         rel="noopener noreferrer"
       >
-        <TwitterIcon className="h-[26px] w-[26px]" />
+        <TwitterIcon className="h-[28px] w-[28px] transition-transform duration-200 ease-in-out hover:translate-y-[-4px]" />
       </a>
       <a
         href="https://reddit.com/r/litecoin"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <RedditIcon className="h-[26px] w-[26px]" />
+        <RedditIcon className="h-[28px] w-[28px] transition-transform duration-200 ease-in-out hover:translate-y-[-4px]" />
       </a>
       <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-        <FacebookIcon className="h-[26px] w-[26px]" />
+        <FacebookIcon className="h-[28px] w-[28px] transition-transform duration-200 ease-in-out hover:translate-y-[-4px]" />
       </a>
       <a
         href="https://github.com/litecoin"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <GithubIcon className="h-[26px] w-[26px]" />
+        <GithubIcon className="h-[28px] w-[28px] transition-transform duration-200 ease-in-out hover:translate-y-[-4px]" />
       </a>
     </div>
   )
