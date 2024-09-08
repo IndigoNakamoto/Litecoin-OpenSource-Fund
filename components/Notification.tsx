35ee50216be3155f7b3bce1623cc794a148dd00b
indigo <indigonakamoto@gmail.com>
2024-09-08 09:45:40 -0700

    Modify notification for custom delay

--- components/Notification.tsx
+++ components/Notification.tsx
@@ -1,14 +1,15 @@
 // components/Notification.js
 import { useEffect } from 'react'
 
-const Notification = ({ message, onClose }) => {
+const Notification = ({ message, onClose, delay = 3000 }) => {
+  // Added delay prop with default value
   useEffect(() => {
     const timer = setTimeout(() => {
       onClose()
-    }, 3000) // 3 seconds
+    }, delay) // Use the customizable delay
 
     return () => clearTimeout(timer) // Cleanup timer on unmount
-  }, [onClose])
+  }, [onClose, delay]) // Add delay to dependencies array
 
   return (
     <div className="animate-slide-up fixed bottom-4 left-1/2 -translate-x-1/2 translate-y-full transform rounded bg-[#f3ccc4] px-8 py-4 font-semibold text-[#222222] shadow-lg transition-transform">
