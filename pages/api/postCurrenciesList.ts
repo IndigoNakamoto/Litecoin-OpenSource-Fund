0e48c81ae799d6e8de5fa1f64dcdb0ef76b1c309
indigo <indigonakamoto@gmail.com>
2024-09-17 22:00:55 -0700

    Revert api method to post

--- pages/api/postCurrenciesList.ts
+++ pages/api/postCurrenciesList.ts
@@ -8,7 +8,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method !== 'GET') {
+  if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method not allowed' })
   }
 
