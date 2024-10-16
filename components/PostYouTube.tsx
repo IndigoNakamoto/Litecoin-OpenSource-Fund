7859de1168606e4405c250cb90bf5a1658c009d4
indigo <indigonakamoto@gmail.com>
2024-10-16 11:31:33 -0700

    Fix: Responsiveness for youtube videos

--- components/PostYouTube.tsx
+++ components/PostYouTube.tsx
@@ -1,15 +1,18 @@
+import React from 'react'
+
 function PostYouTube({ YouTubeID }) {
   return (
-    <div className="w-min rounded-xl border border-gray-300 bg-white p-4">
-      <iframe
-        width="518"
-        height="315"
-        src={`https://www.youtube.com/embed/${YouTubeID}?autoplay=0&mute=0`}
-        title="YouTube video player"
-        frameBorder="0"
-        allow="autoplay; encrypted-media"
-        allowFullScreen
-      ></iframe>
+    <div className="w-full max-w-[550px] rounded-xl border border-gray-300 bg-white p-4">
+      <div className="aspect-video">
+        <iframe
+          className="h-full w-full rounded-md"
+          src={`https://www.youtube.com/embed/${YouTubeID}?autoplay=0&mute=0`}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="autoplay; encrypted-media"
+          allowFullScreen
+        ></iframe>
+      </div>
     </div>
   )
 }
