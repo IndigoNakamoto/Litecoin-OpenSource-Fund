a036d68e4fc5c85ddfd289b1d00ea81fe9bc9a85
indigo <indigonakamoto@gmail.com>
2024-11-07 04:47:22 -0800

    Minor: add space to console

--- components/PaymentModalPersonalInfo.tsx
+++ components/PaymentModalPersonalInfo.tsx
@@ -594,7 +594,7 @@ const PaymentModalPersonalInfo: React.FC<
                       const currentUrl = window.location.href
                       console.log('Current url for sign in: ', currentUrl)
                       const url = new URL(currentUrl)
-                      console.log('URL for twitter callback', url)
+                      console.log('URL for twitter callback ', url)
                       url.searchParams.set('modal', 'true')
                       signIn('twitter', { callbackUrl: url.toString() })
                     }}
