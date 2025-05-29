6d8ffe7928e01a9e0995897276677b407a6c2d23
indigo <indigonakamoto@gmail.com>
2025-05-29 13:15:52 -0700

    feat: Fix OpenGraph meta image URLs in SEOHead component

--- components/SEOHead.tsx
+++ components/SEOHead.tsx
@@ -7,24 +7,24 @@ type SEOHeadProps = {
   coverImage: string
 }
 
-const SEOHead: React.FC<SEOHeadProps> = ({ title, summary, coverImage }) => (
-  <Head>
-    <title>Litecoin | {title}</title>
-    <meta property="og:title" content={title} />
-    <meta property="og:description" content={summary} />
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:site" content="@LTCFoundation" />
-    <meta name="twitter:title" content={title} />
-    <meta name="twitter:description" content={summary} />
-    <meta
-      name="twitter:image"
-      content={`https://www.litecoin.com${coverImage}`}
-    />
-    <meta
-      property="og:image"
-      content={`https://www.litecoin.com${coverImage}`}
-    />
-  </Head>
-)
+const SEOHead: React.FC<SEOHeadProps> = ({ title, summary, coverImage }) => {
+  const imageUrl = coverImage.startsWith('http')
+    ? coverImage
+    : `https://www.litecoin.com${coverImage}`
+
+  return (
+    <Head>
+      <title>Litecoin | {title}</title>
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={summary} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@LTCFoundation" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={summary} />
+      <meta name="twitter:image" content={imageUrl} />
+      <meta property="og:image" content={imageUrl} />
+    </Head>
+  )
+}
 
 export default SEOHead
